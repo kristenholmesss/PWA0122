b12fad128418a5daf3d8eaacbf392244392bca7d
kristenholmesss <61667626+kristenholmesss@users.noreply.github.com>
2022-03-02 16:42:29 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,6 +13,7 @@ var PRECACHE_URLS = [
   'images/hello-icon-196maskable.png',
   'images/hello-icon-256.png',
   'images/hello-icon-512.png',
+  'images/cardinal.png',
   'favicon.ico',
   'sw.js'
   ];
